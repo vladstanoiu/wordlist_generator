7e6688e4b8e309c29a956460347948d98669a90d
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2024-10-16 19:37:35 +0300

    Update dates.py

--- dates.py
+++ dates.py
@@ -1,11 +1,11 @@
 # birthdays - 8 numbers
 
 # input first number
-first_number = input("Enter a number: ")  # Se ia ca string
+first_number = input("Enter a number: ")  # take a string
 
-# check if  the first number has 10 chars
+# check if  the first number has 8 chars
 if len(first_number) != 8:
-    print("First number needs to have 10 chars")
+    print("First number needs to have 8 chars")
 else:
     # last number
     last_number = '99999999'
@@ -15,6 +15,6 @@ else:
         # write file
         for number in range(int(first_number), int(last_number) + 1):
             # write every number in a new line
-            file.write(f"{number:08}\n")  # Format to be 10 chars
+            file.write(f"{number:08}\n")  # Format to be 8 chars
 
     print("Wordlist generated in birthdays.txt")
